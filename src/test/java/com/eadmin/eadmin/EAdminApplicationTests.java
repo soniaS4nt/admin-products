7c88b457ab2ecd49bbefea2872fbab373f0e155e
“sonia” <soniachaparro21@gmail.com>
2023-05-22 16:05:39 -0400

    first commit

---
+++ src/test/java/com/eadmin/eadmin/EAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eadmin.eadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
