f0e5f310b0912379ff6d74a9210e91180fe895da
“sonia” <soniachaparro21@gmail.com>
2023-05-22 20:40:20 -0400

    add constructor, getter and setter to Product model

--- src/main/java/com/eadmin/eadmin/models/Product.java
+++ src/main/java/com/eadmin/eadmin/models/Product.java
@@ -1,25 +1,44 @@
 package com.eadmin.eadmin.models;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.math.BigInteger;
 
+@Data
 @Document(collection = "products")
     public class Product {
+    /*    @Id
+            private UUID id = UUID.randomUUID();*/
+    @Id
+    private  String id;
+    private  String name;
+    private  int price;
 
-       /*    @Id
-         private UUID id = UUID.randomUUID();*/
-        @Id
-        @Getter @Setter
-        private  String id;
-
-        @Getter @Setter
-        private  String name;
-
-        @Getter @Setter
-        private  BigInteger price;
-
+    public String getId() {
+        return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public Product(String id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
