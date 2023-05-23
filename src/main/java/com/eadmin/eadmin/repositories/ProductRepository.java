4917e9b155a82ddccc3d2d9773b1c5c312b9baa2
“sonia” <soniachaparro21@gmail.com>
2023-05-22 20:00:15 -0400

    add ProductRepository

---
+++ src/main/java/com/eadmin/eadmin/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.eadmin.eadmin.repositories;
+
+import com.eadmin.eadmin.models.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
