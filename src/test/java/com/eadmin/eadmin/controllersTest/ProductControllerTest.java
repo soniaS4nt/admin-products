f3fe5b5c1113cef01fe37b9f44d98dcf331d33a4
“sonia” <soniachaparro21@gmail.com>
2023-05-22 21:52:45 -0400

    add shouldGetAllProducts in ProductControllerTest

--- src/test/java/com/eadmin/eadmin/controllersTest/ProductControllerTest.java
+++ src/test/java/com/eadmin/eadmin/controllersTest/ProductControllerTest.java
@@ -3,37 +3,74 @@ package com.eadmin.eadmin.controllersTest;
 import com.eadmin.eadmin.controllers.ProductController;
 import com.eadmin.eadmin.models.Product;
 import com.eadmin.eadmin.services.ProductService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 public class ProductControllerTest {
 
     @MockBean
     private ProductService productService;
+    @InjectMocks
+    private ProductController productController;
+
 
     @Test
     void shouldCreateProduct() {
         // Arrange
         Product productToCreate = new Product("1", "Product 1", 1000);
-        Mockito.when(productService.createProduct(Mockito.any(Product.class))).thenReturn(productToCreate);
+        when(productService.createProduct(Mockito.any(Product.class))).thenReturn(productToCreate);
         ProductController productController = new ProductController(productService);
 
         // Act
         ResponseEntity<Product> response = productController.createProduct(productToCreate);
 
         // Assert
-        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
         Product createdProduct = response.getBody();
-        Assertions.assertNotNull(createdProduct);
-        Assertions.assertEquals("1", createdProduct.getId());
-        Assertions.assertEquals("Product 1", createdProduct.getName());
-        Assertions.assertEquals(1000, createdProduct.getPrice());
+        assertNotNull(createdProduct);
+        assertEquals("1", createdProduct.getId());
+        assertEquals("Product 1", createdProduct.getName());
+        assertEquals(1000, createdProduct.getPrice());
     }
 
+
+
+
+    @Test
+    void shouldGetAllProducts() {
+        // Arrange
+        ProductService productService = mock(ProductService.class);
+        ProductController productController = new ProductController(productService);
+
+        List<Product> products = Arrays.asList(
+                new Product("1", "Product 1", 1000),
+                new Product("2", "Product 2", 2000),
+                new Product("3", "Product 3", 3000)
+        );
+        when(productService.getAllProducts()).thenReturn(products);
+
+        // Act
+        ResponseEntity<List<Product>> response = productController.getAllProducts();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(products, response.getBody());
+    }
 }
+
+
+
+
