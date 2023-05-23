3e78019b9c88d070950415f622b361715e385803
“sonia” <soniachaparro21@gmail.com>
2023-05-22 20:03:23 -0400

    add ProductService

---
+++ src/main/java/com/eadmin/eadmin/services/ProductService.java
@@ -0,0 +1,45 @@
+package com.eadmin.eadmin.services;
+
+import com.eadmin.eadmin.models.Product;
+import com.eadmin.eadmin.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(String id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Product no found"));
+    }
+
+    public Product updateProduct(String id, Product updatedProduct) {
+        Product existingProduct = getProductById(id);
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setPrice(updatedProduct.getPrice());
+        return productRepository.save(existingProduct);
+    }
+
+    public void deleteProduct(String id) {
+        productRepository.deleteById(id);
+    }
+}
